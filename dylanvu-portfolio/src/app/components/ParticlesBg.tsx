5ac946bdf03cf988ed9da1ae7345355d12eff716
Dylan Vu <dylanvu9@gmail.com>
2023-12-04 15:08:28 -0800

    remove scrolling effect... for now?

--- dylanvu-portfolio/src/app/components/ParticlesBg.tsx
+++ dylanvu-portfolio/src/app/components/ParticlesBg.tsx
@@ -1,5 +1,5 @@
 // code taken from react-particles documentation: https://github.com/tsparticles/react
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 import type { Container, Engine } from "tsparticles-engine";
 import Particles from "react-particles";
 import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
@@ -7,12 +7,10 @@ import "../styles/particles-bg.css";
 
 export default function ParticlesBg() {
     const particlesInit = useCallback(async (engine: Engine) => {
-        console.log(engine);
 
         // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
         // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
         // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadFull(engine);
         await loadSlim(engine);
     }, []);
 
@@ -20,49 +18,6 @@ export default function ParticlesBg() {
         console.log(container);
     }, []);
 
-    const [particleDirection, setParticleDirection] = useState<number | "right" | "left" | "bottom" | "bottomLeft" | "bottomRight" | "none" | "top" | "topLeft" | "topRight" | "outside" | "inside" | undefined>("bottom");
-    const [particleSpeed, setParticleSpeed] = useState(2);
-    const [prevScrollPos, setPrevScrollPos] = useState(0);
-
-
-    function onScroll() {
-        const currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
-        if (currentScrollPos > prevScrollPos) {
-            setParticleDirection("top");
-        } else {
-            setParticleDirection("bottom");
-        }
-        console.log(currentScrollPos);
-        setPrevScrollPos(currentScrollPos);
-        setParticleSpeed(6);
-    }
-
-    function onScrollStop() {
-        setParticleDirection("bottom");
-        setParticleSpeed(2);
-    }
-
-    useEffect(() => {
-        let scrollTimeout: NodeJS.Timeout;
-        const handleScroll = () => {
-            // Clear the previous timeout (if any)
-            clearTimeout(scrollTimeout);
-
-            // speed up the particle movement and flip the direction
-            onScroll();
-
-            // a timeout to determine when scrolling has stopped
-            scrollTimeout = setTimeout(function () {
-                onScrollStop();
-            }, 200); // Adjust the timeout value as needed
-        }
-        // attach some fancy listeners to affect the particle direction and speed
-        // window.addEventListener('scroll', handleScroll);
-        // clean up
-        return () => {
-            // window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
 
     return (
         <div className="particle-container">
@@ -78,21 +33,17 @@ export default function ParticlesBg() {
                     },
                     fpsLimit: 120,
                     particles: {
-                        // pass in any initial particles
-                        array: [
-
-                        ],
                         color: {
                             value: "#ffffff",
                         },
                         move: {
-                            direction: particleDirection,
+                            direction: "bottom",
                             enable: true,
                             outModes: {
                                 default: "out",
                             },
                             random: false,
-                            speed: particleSpeed,
+                            speed: 2,
                             straight: false,
                         },
                         number: {
@@ -106,7 +57,7 @@ export default function ParticlesBg() {
                             value: 0.5,
                         },
                         shape: {
-                            type: "circle",
+                            type: "square",
                         },
                         size: {
                             value: { min: 1, max: 5 },
