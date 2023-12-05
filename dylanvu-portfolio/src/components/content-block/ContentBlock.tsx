106cc6b6804eeaf02cf88fd5fd357e3e2dfdbde1
Dylan Vu <dylanvu9@gmail.com>
2023-12-05 14:39:17 -0800

    add styling for content block and parsing between text and bullet in paragraph

--- dylanvu-portfolio/src/components/content-block/ContentBlock.tsx
+++ dylanvu-portfolio/src/components/content-block/ContentBlock.tsx
@@ -1,10 +1,11 @@
 import Paragraph from "./Paragraph";
+import "../../styles/content-block/content-block.css"
 
 export default function ContentBlock({ title, paragraphs }: { title: string, paragraphs: string[] }) {
     return (
         <div>
             {/* header/title */}
-            <div>
+            <div className="content-block-title">
                 {title}
             </div>
             {paragraphs.map((paragraph, index) => <Paragraph key={"_" + title + index.toString()} text={paragraph} />)}
--- dylanvu-portfolio/src/components/content-block/Paragraph.tsx
+++ dylanvu-portfolio/src/components/content-block/Paragraph.tsx
@@ -1,9 +1,31 @@
 
+import "../../styles/content-block/paragraph.css";
 export default function Paragraph({ text }: { text: string }) {
 
+    function formatText(text: string) {
+        // text can either be a paragraph, a link to an image, an external link, or a list/bulletpoint
+        if (text.substring(0, 2) === "* ") {
+            return (
+                // list element
+                <li className="paragraph-bullet">
+                    {text.substring(2)}
+                </li>
+            )
+            // TOOD: handle image and external link
+        } else {
+            return (
+                // just render as text
+                <div className="paragraph-text">
+                    {text}
+                </div>
+            )
+        }
+    }
+
     return (
-        <div>
-            {text}
+        <div className="paragraph">
+            {formatText(text)}
         </div>
     )
+
 }
