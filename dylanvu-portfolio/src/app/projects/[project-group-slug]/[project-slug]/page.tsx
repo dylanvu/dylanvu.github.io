0df5c400f115be0e1a7267f67bc0da110d6ce3c8
Dylan Vu <dylanvu9@gmail.com>
2023-12-04 16:45:54 -0800

    implementing dynamic routes for projects

---
+++ dylanvu-portfolio/src/app/projects/[project-group-slug]/[project-slug]/page.tsx
@@ -0,0 +1,8 @@
+export default function ProjectGroupPage() {
+    // generate project pages based on the json located in public
+    return (
+        <div>
+            Some Project
+        </div>
+    )
+}
---
+++ dylanvu-portfolio/src/app/projects/[project-group-slug]/page.tsx
@@ -0,0 +1,8 @@
+export default function ProjectGroupPage() {
+    // generate list of projects based on the json located in public
+    return (
+        <div>
+            Some Project Group
+        </div>
+    )
+}
