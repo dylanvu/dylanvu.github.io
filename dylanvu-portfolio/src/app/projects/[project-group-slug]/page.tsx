8a60d8c93be8e00c2d8a81999366b333a4b27bff
Dylan Vu <dylanvu9@gmail.com>
2023-12-06 02:54:10 -0800

    make fake project pages

--- dylanvu-portfolio/src/app/projects/[project-group-slug]/page.tsx
+++ dylanvu-portfolio/src/app/projects/[project-group-slug]/page.tsx
@@ -1,10 +1,34 @@
 import { PageLayout } from "@/app/layout"
+import NavigationGroup, { navigationObject } from "@/components/NavigationGroup"
 export default function ProjectGroupPage() {
     // generate list of projects based on the json located in public
+
+    const sections: navigationObject[] = [
+        {
+            displaySection: "Project 1",
+            urlSegment: "project-1"
+        },
+        {
+            displaySection: "Project 2",
+            urlSegment: "project-2"
+        },
+        {
+            displaySection: "Project 3",
+            urlSegment: "project-3"
+        },
+        {
+            displaySection: "Project 4",
+            urlSegment: "project-4"
+        }
+    ];
+
     return (
         <PageLayout>
             <div>
-                Some Project Group
+                <NavigationGroup sections={sections} />
+                <div>
+                    Surprise me!
+                </div>
             </div>
         </PageLayout>
     )
