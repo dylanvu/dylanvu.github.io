b7dd693012fb0e243f0cfb9ac847fa1fff54d169
Dylan Vu <dylanvu9@gmail.com>
2023-12-08 15:53:21 -0800

    add opacity animation to project cards

--- dylanvu-portfolio/src/components/project/ProjectCard.tsx
+++ dylanvu-portfolio/src/components/project/ProjectCard.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { navigationObject } from "../NavigationGroup";
 import "../../styles/project-card.css";
 import Image from "next/image";
+import { motion } from "framer-motion"
 
 export default function ProjectCard({ section }: { section: navigationObject }) {
     // use pathname to dynamically link to different subsections
@@ -14,17 +15,20 @@ export default function ProjectCard({ section }: { section: navigationObject }) 
     const imageUrl = `/${baseless}/${section.urlSegment}/${section.urlSegment}.png`
     console.log(imageUrl);
     return (
-        <Link href={`${pathName}/${section.urlSegment}`} className="project-card">
-            <div className="project-card-title">
-                {section.displaySection}
-            </div>
-            <div className="project-card-image-container">
-                {/* https://stackoverflow.com/questions/65169431/how-to-set-the-next-image-component-to-100-height */}
-                <Image src={imageUrl} alt={section.displaySection} objectFit={"contain"} width={0}
-                    height={0}
-                    sizes="100vw"
-                    style={{ width: '100%', height: 'auto' }} />
-            </div>
-        </Link>
+        <motion.span initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}>
+            <Link href={`${pathName}/${section.urlSegment}`} className="project-card">
+                <div className="project-card-title">
+                    {section.displaySection}
+                </div>
+                <div className="project-card-image-container">
+                    {/* https://stackoverflow.com/questions/65169431/how-to-set-the-next-image-component-to-100-height */}
+                    <Image src={imageUrl} alt={section.displaySection} objectFit={"contain"} width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{ width: '100%', height: 'auto' }} />
+                </div>
+            </Link>
+        </motion.span>
     )
 }
