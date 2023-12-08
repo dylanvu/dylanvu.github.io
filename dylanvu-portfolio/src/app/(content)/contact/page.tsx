8cc2240f7f5d9188580a240614e6bcae0d930f07
Dylan Vu <dylanvu9@gmail.com>
2023-12-08 15:33:22 -0800

    add color and hover effect on icons

--- dylanvu-portfolio/src/app/(content)/contact/page.tsx
+++ dylanvu-portfolio/src/app/(content)/contact/page.tsx
@@ -1,9 +1,8 @@
 import ContentBlockTitle from "@/components/content-block/ContentBlockTitle"
 // Import font awesome icons. Follow these instructions: https://fontawesome.com/how-to-use/on-the-web/using-with/react
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons'
 import "../../../styles/contact.css"
 
 
@@ -13,31 +12,34 @@ export default function ResumeSocials() {
             <div>
                 <ContentBlockTitle title="Resume" />
                 <div className="content-block">
-                    Here's my resume!
+                    <a href="https://github.com/dylanvu" target="_blank" rel="noreferrer" className="contact-icon-link">
+                        <FontAwesomeIcon
+                            icon={faFile}
+                            id="resume"
+                            className="contact-icon"
+                        />
+                    </a>
                 </div>
             </div>
             <div>
                 <ContentBlockTitle title="Contact Me" />
                 <div className="content-block">
-                    <a href="https://github.com/dylanvu" target="_blank" rel="noreferrer" className="contact-icon">
+                    <a href="https://github.com/dylanvu" target="_blank" rel="noreferrer" className="contact-icon-link">
                         <FontAwesomeIcon
                             icon={faGithub}
-                            id="github"
-                            className="icon"
+                            className="contact-icon"
                         />
                     </a>
-                    <a href="https://www.linkedin.com/in/dylanvu9/" target="_blank" rel="noreferrer" className="contact-icon">
+                    <a href="https://www.linkedin.com/in/dylanvu9/" target="_blank" rel="noreferrer" className="contact-icon-link">
                         <FontAwesomeIcon
                             icon={faLinkedinIn}
-
-                            className="icon"
+                            className="contact-icon"
                         />
                     </a>
-                    <a href="mailto:dylanvu9@gmail.com" target="_blank" rel="noreferrer" className="contact-icon">
+                    <a href="mailto:dylanvu9@gmail.com" target="_blank" rel="noreferrer" className="contact-icon-link">
                         <FontAwesomeIcon
                             icon={faEnvelope}
-
-                            className="icon"
+                            className="contact-icon"
                         />
                     </a>
                 </div>
