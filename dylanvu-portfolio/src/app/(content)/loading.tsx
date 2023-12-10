05e5d879d07bb60cfcbe1d27f172f38d970519df
Dylan Vu <dylanvu9@gmail.com>
2023-12-09 16:27:00 -0800

    try to add loading animation but doesn't seem to work well with framer

---
+++ dylanvu-portfolio/src/app/(content)/loading.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { NextPage } from "next";
+import "../../styles/loading.css";
+
+const LoadingPage: NextPage = () => {
+
+    return (
+        <div>
+            {/* <div className="container">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div> */}
+        </div >
+    );
+};
+
+export default LoadingPage;
