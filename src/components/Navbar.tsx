d2b4da3ae5e18941a71e5ca5b86e11668a30e2a8
Dylan Vu <dylanvu9@gmail.com>
2024-02-01 18:13:06 -0800

    add about to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,9 +44,10 @@ const Navbar = () => {
         // #FE5D26 - orange
         <nav className={`navbar ${responsiveClass}`} id="navbar">
             <Link className="nav-link" href="/"><span className="name">Dylan Vu</span></Link>
+            <Link className="nav-link" href="/about"><span className="name">About</span></Link>
             <Link className="nav-link" href="/projects"><span className="name">Projects</span></Link>
             <Link className="nav-link" href="/Dylan_Vu_Resume.pdf" target="_blank" rel="noreferrer"><span className="name">Resume</span></Link>
-            <Link className="nav0link" href="/contact"><span className="name">Contact</span></Link>
+            <Link className="nav-link" href="/contact"><span className="name">Contact</span></Link>
             {/* <Link className="nav-link" style={{ color: "#2081C3" }} href="/projects"><span>Projects</span></Link>
             <Link className="nav-link" style={{ color: "#FE5D26" }} href="/Dylan_Vu_Resume.pdf" target="_blank" rel="noreferrer"><span>Resume</span></Link>
             <Link className="nav0link" style={{ color: "#6DA34D" }} href="/contact"><span>Contact</span></Link> */}
