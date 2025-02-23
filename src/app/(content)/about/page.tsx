c8d02490ef775e22d6e2adbf243d247d464643c4
Dylan Vu <dylanvu9@gmail.com>
2025-02-23 00:01:22 -0800

    whitespace changes

--- src/app/(content)/about/page.tsx
+++ src/app/(content)/about/page.tsx
@@ -1,4 +1,3 @@
-
 // export const getStaticProps = (async (context) => {
 //     const res = await fetch('./about/bio.json')
 //     const text = await res.json()
@@ -15,64 +14,64 @@ import hackathons from "../../../../public/about/hackathons.json";
 import "../../../styles/content-block/paragraph.css";
 import "../../../styles/content-block/content-block.css";
 
-
 /**
  * Inteface to represent a hackathon in the public/about/hackathon.json file
  */
 interface HackathonItem {
-    name: string, // name of the hackathon
-    awards: string[] // list of awards, if any
+  name: string; // name of the hackathon
+  awards: string[]; // list of awards, if any
 }
 
 export default function About() {
-    // parse the text, separate by newline, and then create many Paragraph blocks
-    const aboutParagraphs: string[] = bio.data.split("\n");
-    // process the hackathons
-    const hackathonItems: HackathonItem[] = hackathons.data;
-    const hackathonList = hackathonItems.map((hackathon) => {
-        let hackathonParagraph = "* " + hackathon.name;
-        const awards = hackathon.awards;
-        if (awards.length > 0) {
-            // concatenate the awards
-            hackathonParagraph = hackathonParagraph + " - ";
-            for (const [index, award] of awards.entries()) {
-                hackathonParagraph += `${award}`;
-                if (index != awards.length - 1) {
-                    hackathonParagraph += ", "
-                }
-            }
+  // parse the text, separate by newline, and then create many Paragraph blocks
+  const aboutParagraphs: string[] = bio.data.split("\n");
+  // process the hackathons
+  const hackathonItems: HackathonItem[] = hackathons.data;
+  const hackathonList = hackathonItems.map((hackathon) => {
+    let hackathonParagraph = "* " + hackathon.name;
+    const awards = hackathon.awards;
+    if (awards.length > 0) {
+      // concatenate the awards
+      hackathonParagraph = hackathonParagraph + " - ";
+      for (const [index, award] of awards.entries()) {
+        hackathonParagraph += `${award}`;
+        if (index != awards.length - 1) {
+          hackathonParagraph += ", ";
         }
-        return hackathonParagraph;
-    });
-    const competedHackathons = hackathonItems.filter((hackathon) => !hackathon.name.toLowerCase().includes("mentor"));
-    const filteredHackathonWinners = competedHackathons.filter((hackathon) => hackathon.awards.length > 0);
-    const filteredHackathonMentorship = hackathonItems.filter((hackathon) => hackathon.name.toLowerCase().includes("mentor"));
+      }
+    }
+    return hackathonParagraph;
+  });
+  const competedHackathons = hackathonItems.filter(
+    (hackathon) => !hackathon.name.toLowerCase().includes("mentor")
+  );
+  const filteredHackathonWinners = competedHackathons.filter(
+    (hackathon) => hackathon.awards.length > 0
+  );
+  const filteredHackathonMentorship = hackathonItems.filter((hackathon) =>
+    hackathon.name.toLowerCase().includes("mentor")
+  );
 
-    return (
-        <div >
-            {/* bio */}
-            <ContentBlock title="About" paragraphs={aboutParagraphs} />
-            {/* view me in the context of... */}
-            {/* <ContentBlockTitle title="I'm a..." /> */}
-            {/* <SpecialityCarousel /> */}
-            {/* hackathons */}
-            <div className="content-block">
-                <ContentBlockTitle title="Hackathon Statistics" />
-                {/* we are not using ContentBlock here because I want to animate the numbers counting up */}
-                <div>
-                    Participated:  {hackathonList.length}
-                </div>
-                <div>
-                    Competed:  {competedHackathons.length}
-                </div>
-                <div>
-                    Awards: {filteredHackathonWinners.length}
-                </div>
-                <div>
-                    Mentored: {filteredHackathonMentorship.length}
-                </div>
-            </div>
-            <ContentBlock title="Hackathon List" paragraphs={hackathonList.reverse()} />
-        </div>
-    )
+  return (
+    <div>
+      {/* bio */}
+      <ContentBlock title="About" paragraphs={aboutParagraphs} />
+      {/* view me in the context of... */}
+      {/* <ContentBlockTitle title="I'm a..." /> */}
+      {/* <SpecialityCarousel /> */}
+      {/* hackathons */}
+      <div className="content-block">
+        <ContentBlockTitle title="Hackathon Statistics" />
+        {/* we are not using ContentBlock here because I want to animate the numbers counting up */}
+        <div>Participated: {hackathonList.length}</div>
+        <div>Competed: {competedHackathons.length}</div>
+        <div>Awards: {filteredHackathonWinners.length}</div>
+        <div>Mentored: {filteredHackathonMentorship.length}</div>
+      </div>
+      <ContentBlock
+        title="Hackathon List"
+        paragraphs={hackathonList.reverse()}
+      />
+    </div>
+  );
 }
