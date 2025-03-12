934d4e7af948ffa04d28da9b0c25f7e219417ded
Dylan Vu <dylanvu9@gmail.com>
2025-03-12 12:56:29 -0700

    styling but not done at all

--- src/components/about/HackathonMap/HackathonMap.tsx
+++ src/components/about/HackathonMap/HackathonMap.tsx
@@ -6,8 +6,16 @@ import L, { IconOptions, LatLngTuple } from "leaflet";
 
 import ContentBlockTitle from "../../content-block/ContentBlockTitle";
 import { HackathonList } from "@/constants/Hackathons";
-import HackathonInformation from "@/interfaces/HackathonInformation";
+import HackathonInformation, {
+  HackathonRole,
+  isHackathonRole,
+  HackathonType,
+  isHackathonType,
+} from "@/interfaces/HackathonInformation";
 import HackathonPopup from "./HackathonPopUp";
+import { useState } from "react";
+
+import "../../../styles/hackathon-map/hackathon-map.css";
 
 const defaultIconLength = 25;
 const defaultIconWidth = 41;
@@ -24,12 +32,31 @@ interface MapPosition {
   center: LatLngTuple;
 }
 
+interface aggregationOptions {
+  roleFilters: Set<HackathonRole>;
+  hackathonTypeFilters: Set<HackathonType>;
+}
+
 // function to aggregate hackathons by city and state
 function aggregateHackathons(
-  hackathons: HackathonInformation[]
+  hackathons: HackathonInformation[],
+  aggregationOptions: aggregationOptions
 ): HackathonInformation[][] {
   const locationsVisited: Record<string, HackathonInformation[]> = {};
-  hackathons.forEach((hackathon) => {
+  // filter out hackathons that fit the aggregation options
+  const filteredHackathons = hackathons.filter((hackathon) => {
+    // return true if the options are true
+    // first, filter by role
+    // see if the current hackathon role is within the aggregationOption roleType
+    let roleMatch = aggregationOptions.roleFilters.has(hackathon.role);
+
+    // then filter by hackathon type
+    let typeMatch = aggregationOptions.hackathonTypeFilters.has(hackathon.type);
+    return roleMatch && typeMatch;
+  });
+
+  // now aggregate by location
+  filteredHackathons.forEach((hackathon) => {
     // if the latitude and longitude combination hasn't been visited yet, create an array for it
     const location = `${hackathon.latitude},${hackathon.longitude}`;
     if (!locationsVisited[location]) {
@@ -50,15 +77,108 @@ export default function HackathonMap() {
   };
   const startingMapPosition = californiaPosition;
 
-  const aggregatedHackathonLocations = aggregateHackathons(HackathonList);
+  // checkbox states
+  const [roleFilters, setRoleFilters] = useState<Set<HackathonRole>>(
+    new Set(["Mentor", "Participant"])
+  );
+  const [hackathonTypeFilters, setHackathonTypeFilters] = useState<
+    Set<HackathonType>
+  >(new Set(["In-Person", "Online"]));
+
+  const aggregatedHackathonLocations = aggregateHackathons(HackathonList, {
+    roleFilters: roleFilters,
+    hackathonTypeFilters: hackathonTypeFilters,
+  });
+
+  function handleFilterChange(
+    event: React.ChangeEvent<HTMLInputElement>,
+    value: HackathonRole | HackathonType
+  ) {
+    if (isHackathonRole(value)) {
+      // if the checkbox is checked, add or remove the role from the set
+      if (event.target.checked) {
+        setRoleFilters((prev) => {
+          const newSet = new Set(prev);
+          newSet.add(value);
+          return newSet;
+        });
+      } else {
+        setRoleFilters((prev) => {
+          const newSet = new Set(prev);
+          newSet.delete(value);
+          return newSet;
+        });
+      }
+    } else if (isHackathonType(value)) {
+      // if the checkbox is checked, add or remove the type from the set
+      if (event.target.checked) {
+        setHackathonTypeFilters((prev) => {
+          const newSet = new Set(prev);
+          newSet.add(value);
+          return newSet;
+        });
+      } else {
+        setHackathonTypeFilters((prev) => {
+          const newSet = new Set(prev);
+          newSet.delete(value);
+          return newSet;
+        });
+      }
+    }
+  }
 
   return (
     <div>
       <ContentBlockTitle title={"Hackathon Map"} />
+      {/* TOOD: Make this into a accordion or something */}
+      <div>Show:</div>
+      <div className="map-filter-container">
+        <span>
+          <input
+            type="checkbox"
+            id="hacker"
+            name="hacker"
+            checked={roleFilters.has("Participant")}
+            onChange={(e) => handleFilterChange(e, "Participant")}
+          />
+          <label htmlFor="hacker">Hacker</label>
+        </span>
+        <span>
+          <input
+            type="checkbox"
+            id="mentorship"
+            name="mentorship"
+            checked={roleFilters.has("Mentor")}
+            onChange={(e) => handleFilterChange(e, "Mentor")}
+          />
+          <label htmlFor="mentorship">Mentor</label>
+        </span>
+        <span>
+          <input
+            type="checkbox"
+            id="inpersonHackathons"
+            name="inpersonHackathons"
+            checked={hackathonTypeFilters.has("In-Person")}
+            onChange={(e) => handleFilterChange(e, "In-Person")}
+          />
+          <label htmlFor="inpersonHackathons">In-Person</label>
+        </span>
+        <span>
+          <input
+            type="checkbox"
+            id="onlineHackathons"
+            name="onlineHackathons"
+            checked={hackathonTypeFilters.has("Online")}
+            onChange={(e) => handleFilterChange(e, "Online")}
+          />
+          <label htmlFor="onlineHackathons">Online</label>
+        </span>
+      </div>
+
       <MapContainer
         center={startingMapPosition.center}
         zoom={startingMapPosition.zoom}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         style={{
           height: "730px",
         }}
@@ -94,6 +214,7 @@ export default function HackathonMap() {
             >
               <Popup autoPan={false}>
                 {/* for every hackathon at this location, add the hackathon information */}
+                {/* TODO: Could use some sort of swiping interface, or just simply show a pop up that takes over the whole screen? */}
                 {aggregatedLocation.map((hackathon, hIndex) => (
                   <HackathonPopup
                     hackathon={hackathon}
@@ -109,8 +230,8 @@ export default function HackathonMap() {
   );
 }
 
+// TODO: make the map mobile compatible... ugh.
 // TODO: When there are multiple hackathons at the same location, first create a circle of bubbles around the location, and when you click on one, it opens up a full-screen pop up with the information
-// TODO: add a way to toggle between hackathons and mentorship on the map, maybe different colors for each category
-// TODO: add a way to toggle between in-person and online hackathons
+// TODO: create fancy animations for the pins upon rerender
 // TODO: add a slider to fast forward through the hackathons, pins can drop over time as an animation progresses to show my hackathon journey over time
 // TODO: make a button to zoom in on California, and then out to the US
--- src/interfaces/HackathonInformation.ts
+++ src/interfaces/HackathonInformation.ts
@@ -1,12 +1,25 @@
 // list of hackathons I've been to, and their information
+
+export type HackathonType = "Online" | "In-Person";
+export type HackathonRole = "Mentor" | "Participant";
+
+// create typeguards for the types
+export function isHackathonType(value: any): value is HackathonType {
+  return value === "Online" || value === "In-Person";
+}
+
+export function isHackathonRole(value: any): value is HackathonRole {
+  return value === "Mentor" || value === "Participant";
+}
+
 export default interface HackathonInformation {
   name: string; // hackathon name
   place: string; // name of the hackathon location
   city: string; // name of the hackathon location
   state: string; // name of the hackathon location
   organizer: string; // name of the club or company organizer
-  type: "Online" | "In-Person";
-  role: "Mentor" | "Participant";
+  type: HackathonType;
+  role: HackathonRole;
   awards: string[]; // list of awards, if any
   date: Date;
   latitude: number; // latitude and longitude of the hackathon host location
