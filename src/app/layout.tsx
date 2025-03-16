26999ab847f5ad2819b5a6af9d395eb68ba46bd6
Dylan Vu <dylanvu9@gmail.com>
2025-03-16 00:33:36 -0700

    move synchronous script out of Head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,12 +41,12 @@ export default function RootLayout({
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossOrigin=""
         />
-        <Script
-          src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-          crossOrigin=""
-        ></Script>
       </Head>
+      <Script
+        src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+        crossOrigin=""
+      />
       <body className={inter.className}>
         <main className="main">
           {/* floating particles in background  */}
